501a6ba3f374cffba7aef2456891754dea188141
coreyfedde <coreysfedde@gmail.com>
2017-07-07 11:27:55 -0400

    Added data for the new Date
    Added the foundation for creating a new date for each second which will
    allow the hands to know where the current time.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,38 @@
 
 $('#first').on("click", function(){ console.log('clicked') })
 
-let bodySelection = d3.select("body");
-let svgSelection = bodySelection.append("svg")
-     .attr("width", 50)
-     .attr("height", 50);
-let circleSelection = svgSelection.append("circle")
-     .attr("cx", 25)
-     .attr("cy", 25)
-     .attr("r", 25)
-     .style("fill", "purple");
+// let bodySelection = d3.select("body");
+// let svgSelection = bodySelection.append("svg")
+//      .attr("width", 50)
+//      .attr("height", 50);
+// let circleSelection = svgSelection.append("circle")
+//      .attr("cx", 25)
+//      .attr("cy", 25)
+//      .attr("r", 25)
+//      .style("fill", "purple");
+
+let fields = function() {
+  let currentTime
+  let hour
+  let minute
+  let second
+  currentTime = new Date()
+  second = currentTime.getSeconds()
+  minute = currentTime.getMinute()
+  hour = currentTime.getHours() + minute / 60
+  return data = [
+    {
+      "unit": "seconds",
+      "numeric": second
+    },
+    {
+      "unit": "minutes",
+      "numeric": minute
+    },
+    {
+      "unit": "hours",
+      "numeric": hour
+    }
+  ]
+
+}
